2369a7a56c467324734de7af3825b7132a4f99c3
niko-holmes <holmesnikolaus@gmail.com>
2025-04-29 12:32:37 +0100

    added basic nvim config

---
+++ .config/nvim/lua/base_config/set.lua
@@ -0,0 +1,15 @@
+vim.opt.nu = true                       -- enable line numbers
+vim.opt.relativenumber = false	        -- dont use relative line numbers
+
+-- set tab size to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+--vim.opt.termguicolors = true
+
+vim.cmd([[match Error /\s\+$/]])        -- highlight trailing whitespaces
+vim.cmd([[match Error /\%>120c/]])      -- highlight lines > 120 chars
