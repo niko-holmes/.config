f6be6a0f57a71107cf12149a9e3fd1d36bf794fa
niko-holmes <holmesnikolaus@gmail.com>
2025-11-26 16:04:29 +0000

    add mason, ALE linters and LSPs auto-install on start up

--- nvim/lua/base_config/lazy_init.lua
+++ nvim/lua/base_config/lazy_init.lua
@@ -15,3 +15,31 @@ require("lazy").setup({
     spec = "base_config.lazy",
     change_detection = { notify = false }
 })
+
+local function install_mason_package(pkg_name)
+    local mason_registry = require("mason-registry")
+    local pkg = mason_registry.get_package(pkg_name)
+    if not pkg:is_installed() then
+        print(string.format("Installing %s", pkg_name))
+        pkg:install()
+    end
+end
+
+-- Install ALE requirements
+for _, linters in pairs(vim.g.ale_linters) do
+    for _, linter_name in pairs(linters) do
+        install_mason_package(linter_name)
+    end
+end
+
+-- Install LSPs
+local required_lsps = {
+    python = {'python-lsp-server'},
+    cpp = {'clangd'},
+    lua = {'lua-language-server'}
+}
+for _, lang_reqs in pairs(required_lsps) do
+    for _, lsp in pairs(lang_reqs) do
+        install_mason_package(lsp)
+    end
+end
